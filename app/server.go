06ec55bf8b984683bfce787980ddebf307ceb1be
Pedro <pepizarro@alumnos.uai.cl>
2024-05-14 16:05:16 -0400

    pass stage

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -18,9 +19,45 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+	fmt.Println("Listening on port 6379")
+	for {
+
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+
+}
+
+func handleConnection(conn net.Conn) {
+	// Add your code to handle the connection here
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		// Read data from the client
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		if strings.Contains(string(buffer[:n]), "PING") {
+			_, err := conn.Write([]byte("+PONG\r\n"))
+			if err != nil {
+				return
+			}
+		}
+
+		// Process and use the data (here, we'll just print it)
+		fmt.Printf("Received: %s\n", buffer[:n])
 	}
 }
