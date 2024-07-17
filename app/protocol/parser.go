57ded66e39260ce68af14a1caed6fd56b0ac36e4
Pedro <pepizarro@alumnos.uai.cl>
2024-07-16 20:08:25 -0400

    codecrafters submit [skip ci]

--- app/protocol/parser.go
+++ app/protocol/parser.go
@@ -6,6 +6,7 @@ type Parser interface {
 	GetParams(buffer []byte) ([][]byte, error)
 	GetArgs(buffer []byte, start int) (map[string][]byte, error)
 	GetType(buffer []byte) (string, error)
+	WriteSimpleString(string) []byte
 	WriteString(string) []byte
 	WriteOk() []byte
 	WriteError(string) []byte
